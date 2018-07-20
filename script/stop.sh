824b1f2b7bd70e4a7c2b6318a6bf8430b35f274d
shuntaka9576 <shuntaka9576@gmail.com>
2018-07-20 15:30:20 +0900

    プロセスが存在しないときに、エラーになりデプロイに失敗するので修正

--- script/stop.sh
+++ script/stop.sh
@@ -1,1 +1,7 @@
 sudo killall helloworld
+count=`ssh-add -L | grep github_office_rsa | grep -v grep | wc -l`
+if [ $count = 0 ]; then
+  killall ssh-agent
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/github_office_rsa
+fi
