b0c726d78dc8738fae423219efc0244011a50d35
shuntaka9576 <shuntaka9576@gmail.com>
2018-07-20 15:41:43 +0900

    update

--- script/stop.sh
+++ script/stop.sh
@@ -1,7 +1,4 @@
-sudo killall helloworld
-count=`ssh-add -L | grep github_office_rsa | grep -v grep | wc -l`
-if [ $count = 0 ]; then
-  killall ssh-agent
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/github_office_rsa
+count=`ps aux| grep helloworld| grep -v grep| wc -l`
+if [ $count != 0 ]; then
+  sudo killall helloworld
 fi
